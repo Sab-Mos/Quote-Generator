6c2c412dfb1de5a9dd196733ccf1c56cff0670f8
Sab-Mos <131381168+Sab-Mos@users.noreply.github.com>
2023-08-12 01:00:19 +0400

    Add files via upload

---
+++ Quote Generator/script.js
@@ -0,0 +1,27 @@
+const api_url = "https://api.quotable.io/random";
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const button = document.querySelector(' .new-quote');
+const tweetBtn =document.querySelector('.tweet');
+
+async function getQuote (url)  {
+  const response = await fetch(url);
+  let data = await response.json();
+  quote.textContent = data.content;
+  author.textContent = data.author;
+};
+
+
+button.addEventListener('click', ()=> getQuote(api_url));
+
+tweetBtn.addEventListener('click', () => {
+    window.open( "https://twitter.com/intent/tweet?text=" +quote.textContent + "---- by " + author.textContent, "Tweet Window", "width = 600 height = 300", )
+})
+
+
+
+
+
+
+
+
